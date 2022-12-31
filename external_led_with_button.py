e4cebfd94fc1d800a21b14cd4343dd96cac1b3ba
hnaussed <holger.naussed@gmail.com>
2022-12-31 23:31:37 +0100

    update

--- external_led_with_button.py
+++ external_led_with_button.py
@@ -1,10 +1,13 @@
 from machine import Pin
 import time
-led_external = Pin(15, Pin.OUT)
+led_external_1 = Pin(15, Pin.OUT)
+led_external_2 = Pin(13, Pin.OUT)
 button = Pin(14, Pin.IN)
 
 while True:
- if button.value() == 1:
-    led_external.value(1)
-    time.sleep(2)
- led_external.value(0)
+   if button.value() == 1:
+      led_external_1.value(1)
+      led_external_2.value(0)
+      time.sleep(2)
+   led_external_1.value(0)
+   led_external_2.value(1)
