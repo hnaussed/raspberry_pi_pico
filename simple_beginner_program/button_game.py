595bd934424336ce7bc699219925502c748622fc
hnaussed <holger.naussed@gmail.com>
2023-01-02 16:48:43 +0100

    some updates

--- simple_beginner_program/button_game.py
+++ simple_beginner_program/button_game.py
@@ -1,14 +1,14 @@
 import machine
-import utime
-import urandom
+import time
+import random
 
 pressed = False
 fastest_button = None
-timer_reaction = 0
 
 led = machine.Pin(15, machine.Pin.OUT)
-left_button = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_DOWN)
+
 right_button = machine.Pin(16, machine.Pin.IN, machine.Pin.PULL_DOWN)
+left_button = machine.Pin(14, machine.Pin.IN, machine.Pin.PULL_DOWN)
 
 def button_handler(pin):
     global pressed
@@ -17,25 +17,21 @@ def button_handler(pin):
         print(pin)
         global fastest_button
         fastest_button = pin
-        global timer_reaction
-        timer_reaction = utime.ticks_diff(utime.ticks_ms(), timer_start)
-
+        timer_reaction = time.ticks_diff(time.ticks_ms(), timer_start)
+        print("Your reaction time was " + str(timer_reaction) +" milliseconds!")
 
 led.value(1)
-utime.sleep(urandom.uniform(5, 10))
+time.sleep(random.uniform(5, 10))
 led.value(0)
 
-timer_start = utime.ticks_ms()
+timer_start = time.ticks_ms()
+
 left_button.irq(trigger=machine.Pin.IRQ_RISING, handler=button_handler)
 right_button.irq(trigger=machine.Pin.IRQ_RISING, handler=button_handler)
 
 while fastest_button is None:
-    utime.sleep(1)
-
+    time.sleep(1)
 if fastest_button is left_button:
     print("Left Player wins!")
 elif fastest_button is right_button:
     print("Right Player wins!")
-
-print("Your reaction time was " + str(timer_reaction) +
-" milliseconds!")
